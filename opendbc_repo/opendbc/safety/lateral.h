eff384439c332fda2790809c5c71684f0be38fbf
yihongfu <245499930@qq.com>
2026-07-28 17:59:50 +0800

    fix(safety): align merged opendbc curvature state

--- opendbc_repo/opendbc/safety/lateral.h
+++ opendbc_repo/opendbc/safety/lateral.h
@@ -306,9 +306,8 @@ bool steer_curvature_cmd_checks(int desired_curvature, int steer_power, bool ste
     violation |= !lat_control_allowed() && steer_control_enabled;
   }
 
-  // reset to zero or measured curvature depending on EPS expectation
-  if (violation || !lat_control_allowed()) {
-    curvature_state.desired_last = limits.inactive_curvature_is_zero ? 0 : curvature_state.meas.values[0];
+  if (violation) {
+    curvature_state.desired_last = 0;
   }
 
   return violation;
