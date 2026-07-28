7bd11f557e68a637348361b57b15182513c19d95
yihongfu <245499930@qq.com>
2026-07-28 18:07:04 +0800

    fix(safety): isolate CAN FD Volkswagen mode

--- opendbc_repo/opendbc/safety/can.h
+++ opendbc_repo/opendbc/safety/can.h
@@ -3,7 +3,12 @@
 static const unsigned char dlc_to_len[] = {0U, 1U, 2U, 3U, 4U, 5U, 6U, 7U, 8U, 12U, 16U, 20U, 24U, 32U, 48U, 64U};
 
 #define CANPACKET_HEAD_SIZE 6U  // non-data portion of CANPacket_t
-#define CANPACKET_DATA_SIZE_MAX 64U
+
+#ifdef CANFD
+  #define CANPACKET_DATA_SIZE_MAX 64U
+#else
+  #define CANPACKET_DATA_SIZE_MAX 8U
+#endif
 
 typedef struct {
   unsigned char fd : 1;
