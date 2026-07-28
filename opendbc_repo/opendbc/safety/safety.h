c3d96cf4e211c16ff6d5ba4645aa763471e28d47
yihongfu <245499930@qq.com>
2026-07-28 18:07:11 +0800

    fix(safety): isolate CAN FD Volkswagen mode

--- opendbc_repo/opendbc/safety/safety.h
+++ opendbc_repo/opendbc/safety/safety.h
@@ -23,7 +23,9 @@
 #include "opendbc/safety/modes/nissan.h"
 #include "opendbc/safety/modes/volkswagen_mlb.h"
 #include "opendbc/safety/modes/volkswagen_mqb.h"
+#ifdef CANFD
 #include "opendbc/safety/modes/volkswagen_meb.h"
+#endif
 #include "opendbc/safety/modes/volkswagen_pq.h"
 #include "opendbc/safety/modes/elm327.h"
 #include "opendbc/safety/modes/body.h"
@@ -458,7 +460,9 @@ int set_safety_hooks(uint16_t mode, uint16_t param) {
     {SAFETY_CHRYSLER, &chrysler_hooks},
     {SAFETY_SUBARU, &subaru_hooks},
     {SAFETY_VOLKSWAGEN_MQB, &volkswagen_mqb_hooks},
+#ifdef CANFD
     {SAFETY_VOLKSWAGEN_MEB, &volkswagen_meb_hooks},
+#endif
     {SAFETY_NISSAN, &nissan_hooks},
     {SAFETY_NOOUTPUT, &nooutput_hooks},
     {SAFETY_HYUNDAI_LEGACY, &hyundai_legacy_hooks},
