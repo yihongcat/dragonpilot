b74e18421d587c7d1245b473ce864f66f3482aa1
yihongfu <245499930@qq.com>
2026-07-28 18:15:21 +0800

    fix(ui): resolve DP assets from nested UI

--- openpilot/selfdrive/ui/layouts/sidebar.py
+++ openpilot/selfdrive/ui/layouts/sidebar.py
@@ -74,7 +74,7 @@ class Sidebar(Widget):
     self._connect_status = MetricData(tr_noop("CONNECT"), tr_noop("OFFLINE"), Colors.WARNING)
     self._recording_audio = False
 
-    self._home_img = gui_app.texture("../../dragonpilot/selfdrive/assets/images/button_home.png", HOME_BTN.width, HOME_BTN.height)
+    self._home_img = gui_app.texture("../../../dragonpilot/selfdrive/assets/images/button_home.png", HOME_BTN.width, HOME_BTN.height)
     self._flag_img = gui_app.texture("images/button_flag.png", HOME_BTN.width, HOME_BTN.height)
     self._settings_img = gui_app.texture("images/button_settings.png", SETTINGS_BTN.width, SETTINGS_BTN.height)
     self._mic_img = gui_app.texture("icons/microphone.png", 30, 30)
